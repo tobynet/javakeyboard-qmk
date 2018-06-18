62ba497f3c3e199ec2d69ed7917e0c551cde06d7
tobynet <toby.netinfo+git@gmail.com>
2018-06-19 01:51:42 +0900

    Fix underglow for nyquist/rev1:j3

--- keyboards/nyquist/keymaps/j3/config.h
+++ keyboards/nyquist/keymaps/j3/config.h
@@ -33,10 +33,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 20
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
+#define RGBLED_NUM 12
+#define RGBLIGHT_HUE_STEP 20
+#define RGBLIGHT_SAT_STEP 32
+#define RGBLIGHT_VAL_STEP 32
 
 /* Audio settings */
 #ifdef AUDIO_ENABLE

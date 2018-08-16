98913f30d08a86910ff095a10eb32eae1ab1e116
tobynet <toby.netinfo+git@gmail.com>
2018-08-16 19:03:28 +0900

    Add Eucalyn layout

--- keyboards/nyquist/keymaps/j3/keymap.c
+++ keyboards/nyquist/keymaps/j3/keymap.c
@@ -12,13 +12,15 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-#define _FN3 3
-#define _FN 4
+#define _EUCALYN 1
+#define _LOWER 2
+#define _RAISE 3
+#define _FN3 4
+#define _FN 5
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
+  EUCALYN,
   LOWER,
   RAISE,
   FN3,
@@ -82,6 +84,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT, \
   CTL_SPC, KC_A,    KC_S,    KC_D,    _L(KC_F),KC_G,    KC_H,    _R(KC_J), KC_K,   KC_L,    KC_SCLN, JP_COLN, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH, \
+  FN,      FN3,     KC_LGUI, KC_LALT, KC_MHEN, KC_SPC,  KC_BSPC, KC_ENT,  KC_HENK, KC_LEFT, KC_DOWN, KC_RGHT \
+),
+
+/* Eucalyn
+ * ,-----------------------------------------------------------------------------------.
+ * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | -or= |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Tab  | /or? | ,or< | .or> |   F  |   Q  |   M  |   R  |   D  |   Y  |   P  | @or` |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Ctrl |   A  |   O  |   E  |I Low |   U  |   G  |T Rais|   K  |   S  |   N  | 'or" |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   W  |   B  |   H  |   J  |   L  |  UP  | ;or+ |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Fn   | FN3  | GUI  | Alt  |Muhen |Space | Bksp |Enter |Henkan| Left | Down |Right |
+ * `-----------------------------------------------------------------------------------'
+ * ref. http://eucalyn.hatenadiary.jp/entry/saikyo-interface
+ * ref. https://yushakobo.jp/blog/pluis9/2017/12/thinkkeylayout/
+ */
+[_EUCALYN] = LAYOUT( \
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
+  KC_TAB,  KC_SLSH, KC_COMM, KC_DOT,  KC_F,    KC_Q,    KC_M,    KC_R,    KC_D,    KC_Y,    KC_P,    JP_AT, \
+  CTL_SPC, KC_A,    KC_O,    KC_E,    _L(KC_I),KC_U,    KC_G,    _R(KC_T),KC_K,    KC_S,    KC_N,    JP_COLN, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_W,    KC_B,    KC_H,    KC_J,    KC_L,    KC_UP,   KC_SCLN,\
   FN,      FN3,     KC_LGUI, KC_LALT, KC_MHEN, KC_SPC,  KC_BSPC, KC_ENT,  KC_HENK, KC_LEFT, KC_DOWN, KC_RGHT \
 ),
 
@@ -156,7 +181,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |  F11 |      |      |ENG_DIC|      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |      |QWERTY|EUCALYN|      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |PG UP |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -166,7 +191,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_FN] = LAYOUT( \
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL, \
   KC_F11,  _______, _______, ENG_DIC, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, QWERTY,  EUCALYN, _______, _______, _______, \
   _______, KC_JANT, KC_JAPL, KC_JAVA, KC_JADT, KC_JAPU, _______, _______, _______, _______, KC_PGUP, _______, \
   _______, KC_DLJA, _______, _______, _______, _______, RESET,   _______, _______, KC_HOME, KC_PGDN, KC_END \
 )
@@ -212,6 +237,8 @@ float tone_song2[][2] = SONG(FAST_ONE_UP_SOUND);
 float tone_song3[][2] = SONG(ZELDA_PUZZLE);
 float tone_song4[][2] = SONG(TREASURE_SOUND);
 float tone_song5[][2] = SONG(DOORCHIME_SOUND);
+float tone_qwerty[][2] = SONG(QWERTY_SOUND);
+float tone_eucalyn[][2]  = SONG(COLEMAK_SOUND);
 #endif
 
 
@@ -222,6 +249,26 @@ void persistent_default_layer_set(uint16_t default_layer) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        #ifdef AUDIO_ENABLE
+          PLAY_SONG(tone_qwerty);
+        #endif
+        persistent_default_layer_set(1UL<<_QWERTY);
+      }
+      return false;
+      break;
+
+    case EUCALYN:
+      if (record->event.pressed) {
+        #ifdef AUDIO_ENABLE
+          PLAY_SONG(tone_eucalyn);
+        #endif
+        persistent_default_layer_set(1UL<<_EUCALYN);
+      }
+      return false;
+      break;
+
     case FN:
       if (record->event.pressed) {
         layer_on(_FN);

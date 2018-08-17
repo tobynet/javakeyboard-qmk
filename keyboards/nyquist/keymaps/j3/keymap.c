6bf756139b9bd0404a21aa1e29e8c4804162b61a
tobynet <toby.netinfo+git@gmail.com>
2018-08-17 17:53:05 +0900

    Reduce mistakes of left ctrl key

--- keyboards/nyquist/keymaps/j3/keymap.c
+++ keyboards/nyquist/keymaps/j3/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT( \
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT, \
-  CTL_SPC, KC_A,    KC_S,    KC_D,    _L(KC_F),KC_G,    KC_H,    _R(KC_J), KC_K,   KC_L,    KC_SCLN, JP_COLN, \
+  KC_LCTL, KC_A,    KC_S,    KC_D,    _L(KC_F),KC_G,    KC_H,    _R(KC_J), KC_K,   KC_L,    KC_SCLN, JP_COLN, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH, \
   FN,      FN3,     KC_LGUI, KC_LALT, KC_MHEN, KC_SPC,  KC_BSPC, KC_ENT,  KC_HENK, KC_LEFT, KC_DOWN, KC_RGHT \
 ),
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_EUCALYN] = LAYOUT( \
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   KC_TAB,  KC_SLSH, KC_COMM, KC_DOT,  KC_F,    KC_Q,    KC_M,    KC_R,    KC_D,    KC_Y,    KC_P,    JP_AT, \
-  CTL_SPC, KC_A,    KC_O,    KC_E,    _L(KC_I),KC_U,    KC_G,    _R(KC_T),KC_K,    KC_S,    KC_N,    JP_COLN, \
+  KC_LCTL, KC_A,    KC_O,    KC_E,    _L(KC_I),KC_U,    KC_G,    _R(KC_T),KC_K,    KC_S,    KC_N,    JP_COLN, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_W,    KC_B,    KC_H,    KC_J,    KC_L,    KC_UP,   KC_SCLN,\
   FN,      FN3,     KC_LGUI, KC_LALT, KC_MHEN, KC_SPC,  KC_BSPC, KC_ENT,  KC_HENK, KC_LEFT, KC_DOWN, KC_RGHT \
 ),

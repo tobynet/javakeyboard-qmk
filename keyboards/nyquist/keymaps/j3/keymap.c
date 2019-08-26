ba25a18d623b44c4a7764c4f790111f481442b16
tobynet <toby.netinfo+git@gmail.com>
2019-08-26 11:44:04 +0900

    Improve layout for without thumbs

--- keyboards/nyquist/keymaps/j3/keymap.c
+++ keyboards/nyquist/keymaps/j3/keymap.c
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | @or` |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl |   A  |  S   |   D  |F Low |   G  |   H  |J Rais|   K  |   L  | ;or+ | 'or" |
+ * | Ctrl |A Rais|  S   |   D  |F Low |   G  |   H  |J Rais|   K  |   L  |;+ Low| 'or" |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  | ,or< | .or> |  UP  | /or? |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -87,9 +87,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT( \
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT, \
-  KC_LCTL, KC_A,    KC_S,    KC_D,    _L(KC_F),KC_G,    KC_H,    _R(KC_J), KC_K,   KC_L,    KC_SCLN, JP_COLN, \
+  KC_LCTL, _R(KC_A),KC_S,    KC_D,    _L(KC_F),KC_G,    KC_H,    _R(KC_J), KC_K,   KC_L,  _L(KC_SCLN),JP_COLN, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH, \
-  FN3,     FN,     KC_LGUI, KC_LALT,_G(KC_MHEN),KC_SPC,  KC_BSPC, KC_ENT,  KC_HENK, KC_LEFT, KC_DOWN, KC_RGHT \
+  FN3,     FN,     KC_LGUI, KC_LALT,_G(KC_MHEN),KC_SPC, KC_BSPC, KC_ENT,  KC_HENK, KC_LEFT, KC_DOWN, KC_RGHT \
 ),
 
 /* Qwerty based for gaming
@@ -140,7 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |  F11 |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |      |      |      |      |      |      | Bksp |Enter |Henkan|      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |PG UP |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -150,7 +150,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT( \
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
   KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, KC_BSPC, KC_ENT,  KC_HENK, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END  \
 ),
@@ -161,7 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | ESC  |  !   |   "  |   #  |   $  |   %  |   &  |   '  |   (  |   )  | ^or~ | \or| |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |   [  |   ]  |   :  |  '   |
+ * |      |      | GUI  | Alt  |Muhen |Space |      |      |   [  |   ]  |   :  |  '   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |   {  |   }  |      |  _   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -171,7 +171,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
   _______, JP_EXLM, JP_DQT,  JP_HASH, JP_DLR , JP_PERC, JP_AMPR, JP_QUOT, JP_LPRN, JP_RPRN, JP_CIRC, JP_YEN, \
   KC_ESC,  JP_EXLM, JP_DQT,  JP_HASH, JP_DLR , JP_PERC, JP_AMPR, JP_QUOT, JP_LPRN, JP_RPRN, JP_CIRC, JP_YEN, \
-  _______, _______, _______, _______, _______, _______, _______, _______, JP_LBRC, JP_RBRC, JP_COLN, JP_QUOT, \
+  _______, _______, KC_LGUI, KC_LALT, KC_MHEN, KC_SPC,  _______, _______, JP_LBRC, JP_RBRC, JP_COLN, JP_QUOT, \
   _______, _______, _______, _______, _______, _______, _______, _______, JP_LCBR, JP_RCBR, _______, JP_UNDS, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),

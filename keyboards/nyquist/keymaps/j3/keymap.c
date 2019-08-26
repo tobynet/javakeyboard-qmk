c54bb9d95123c454b0e36f9451118b1a86d15266
tobynet <toby.netinfo+git@gmail.com>
2019-08-26 10:44:14 +0900

    Remove a layer because of reducing firmware file size

--- keyboards/nyquist/keymaps/j3/keymap.c
+++ keyboards/nyquist/keymaps/j3/keymap.c
@@ -12,7 +12,6 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QWERTY 0
-#define _EUCALYN 1
 #define _GAMING 2
 #define _GAMING_FN 3
 #define _LOWER 4
@@ -22,7 +21,6 @@ extern keymap_config_t keymap_config;
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  EUCALYN,
   GAMING,
   GAMING_FN,
   LOWER,
@@ -92,28 +90,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LCTL, KC_A,    KC_S,    KC_D,    _L(KC_F),KC_G,    KC_H,    _R(KC_J), KC_K,   KC_L,    KC_SCLN, JP_COLN, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH, \
   FN3,     FN,     KC_LGUI, KC_LALT,_G(KC_MHEN),KC_SPC,  KC_BSPC, KC_ENT,  KC_HENK, KC_LEFT, KC_DOWN, KC_RGHT \
-),
-
-/* Eucalyn 2018-03
- * ,-----------------------------------------------------------------------------------.
- * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | -or= |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   Q  |   W  | ,or< | .or> | ;or+ |   M  |   R  |   D  |   Y  |   P  | @or` |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl |   A  |   O  |   E  |I Low |   U  |   G  |T Rais|   K  |   S  |   N  | :or* |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   F  |   B  |   H  |   J  |   L  |  UP  | /or? |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Fn3  |  FN  | GUI  | Alt  |Muhen |Space | Bksp |Enter |Henkan| Left | Down |Right |
- * `-----------------------------------------------------------------------------------'
- * ref. http://eucalyn.hatenadiary.jp/entry/about-eucalyn-layout
- */
-[_EUCALYN] = LAYOUT( \
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
-  KC_TAB,  KC_Q,    KC_W,    KC_COMM, KC_DOT,  KC_SCLN, KC_M,    KC_R,    KC_D,    KC_Y,    KC_P,    JP_AT, \
-  KC_LCTL, KC_A,    KC_O,    KC_E,    _L(KC_I),KC_U,    KC_G,    _R(KC_T),KC_K,    KC_S,    KC_N,    JP_COLN, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_F,    KC_B,    KC_H,    KC_J,    KC_L,    KC_UP,   KC_SLSH, \
-  FN3,     FN,      KC_LGUI, KC_LALT, KC_MHEN, KC_SPC,  KC_BSPC, KC_ENT,  KC_HENK, KC_LEFT, KC_DOWN, KC_RGHT \
 ),
 
 /* Qwerty based for gaming
@@ -229,7 +205,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |  F11 |      |      |ENG_DIC|      |      |      |      |      |      |     |RESET |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |QWERTY|EUCALYN|GAMING|     |      |
+ * |      |      |      |      |      |      |      |QWERTY|GAMING|     |     |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |PG UP |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -239,7 +215,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_FN] = LAYOUT( \
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL, \
   KC_F11,  _______, _______, ENG_DIC, _______, _______, _______, _______, _______, _______, _______, RESET , \
-  _______, _______, _______, _______, _______, _______, _______, QWERTY,  EUCALYN, GAMING,  _______, KC_DLJA, \
+  _______, _______, _______, _______, _______, _______, _______, QWERTY,  GAMING,  _______, _______, KC_DLJA, \
   _______, KC_JANT, KC_JAPL, KC_JAVA, KC_JADT, KC_JAPU, _______, _______, _______, _______, KC_PGUP, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END \
 )
@@ -286,7 +262,6 @@ float tone_song3[][2] = SONG(ZELDA_PUZZLE);
 float tone_song4[][2] = SONG(TREASURE_SOUND);
 float tone_song5[][2] = SONG(DOORCHIME_SOUND);
 float tone_qwerty[][2] = SONG(QWERTY_SOUND);
-float tone_eucalyn[][2]  = SONG(COLEMAK_SOUND);
 float tone_gaming[][2] = SONG(DVORAK_SOUND);
 #endif
 
@@ -304,16 +279,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           PLAY_SONG(tone_qwerty);
         #endif
         persistent_default_layer_set(1UL<<_QWERTY);
-      }
-      return false;
-      break;
-
-    case EUCALYN:
-      if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(tone_eucalyn);
-        #endif
-        persistent_default_layer_set(1UL<<_EUCALYN);
       }
       return false;
       break;

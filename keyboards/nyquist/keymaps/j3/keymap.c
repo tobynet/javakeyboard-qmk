c03cfded0bf2ff1fae4bca8cb6d28ed35ab26904
tobynet <toby.netinfo+git@gmail.com>
2020-03-01 08:04:24 +0900

    Add Numpad Layer

--- keyboards/nyquist/keymaps/j3/keymap.c
+++ keyboards/nyquist/keymaps/j3/keymap.c
@@ -12,6 +12,7 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QWERTY 0
+#define _NUMPAD 1
 #define _GAMING 2
 #define _GAMING_FN 3
 #define _LOWER 4
@@ -21,6 +22,7 @@ extern keymap_config_t keymap_config;
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
+  NUMPAD,
   GAMING,
   GAMING_FN,
   LOWER,
@@ -53,6 +55,9 @@ enum custom_keycodes {
 #define _R(x) LT(_RAISE, x)
 
 // Modifiers for One hand
+#define _N(x) LT(_NUMPAD, x)
+
+// Modifiers for One hand
 #define _G(x) LT(_GAMING_FN, x)
 
 // Mod Tap ref. https://docs.qmk.fm/#/feature_advanced_keycodes?id=mod-tap
@@ -81,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  | ,or< | .or> |  UP  | /or? |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Fn3  |  FN  | GUI  | Alt  |G(Muhen)|Space|Bksp |Enter |Henkan| Left | Down |Right |
+ * | Fn3  |  FN  | GUI  | Alt  |N(Muhen)|Space|Bksp |Enter |Henkan| Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT( \
@@ -89,7 +94,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT, \
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    _R(KC_J), KC_K,   KC_L,    KC_SCLN, JP_COLN, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH, \
-  FN3,     FN,     KC_LGUI, KC_LALT,_G(KC_MHEN),KC_SPC, KC_BSPC, KC_ENT,  KC_HENK, KC_LEFT, KC_DOWN, KC_RGHT \
+  FN3,     FN,     KC_LGUI, KC_LALT,_N(KC_MHEN),KC_SPC, KC_BSPC, KC_ENT,  KC_HENK, KC_LEFT, KC_DOWN, KC_RGHT \
+),
+
+/* Numpad( for the left hand)
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      | Num/ | Num* | Num- |      |      |      \      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      | Num7 | Num8 | Num9 | Num- |      |      |      \      |      |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      | Num4 | Num5 | Num6 | Num+ |      |      |      \      |      |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      | Num1 | Num2 | Num3 |NumENT|      |      |      \      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      | Num0 |      | Num. |      |      |      |      \      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_NUMPAD] = LAYOUT( \
+  _______, _______, KC_PSLS, KC_PAST, KC_PMNS, _______,  _______, _______, _______, _______, _______, _______, \
+  _______, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, _______,  _______, _______, _______, _______, _______, _______, \
+  _______, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______,  _______, _______, _______, _______, _______, _______, \
+  _______, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______,  _______, _______, _______, _______, _______, _______,\
+  _______, KC_P0,   _______, KC_PDOT, _______, _______,  _______, _______, _______, _______, _______, _______ \
 ),
 
 /* Qwerty based for gaming
